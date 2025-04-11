edc2edfcd628c66eb231ff770556a810ed628461
Ezequiel-B18 <eablajevitch@gmail.com>
2025-04-11 12:07:39 -0300

    Implementacion de tests para no palindromos

--- tests/test_palindrome.py
+++ tests/test_palindrome.py
@@ -2,6 +2,7 @@ import unittest
 from src.palindrome import is_palindrome
 
 class TestPalindrome(unittest.TestCase):
+    
     def test_simple_palindromes(self):
         self.assertTrue(is_palindrome("madam"))
         self.assertTrue(is_palindrome("racecar"))
@@ -10,6 +11,10 @@ class TestPalindrome(unittest.TestCase):
         self.assertTrue(is_palindrome("A man, a plan, a canal: Panama"))
         self.assertTrue(is_palindrome("Was it a car or a cat I saw?"))
         self.assertTrue(is_palindrome("No lemon, no melon"))
+    def test_non_palindromes(self):
+        self.assertFalse(is_palindrome("hello"))
+        self.assertFalse(is_palindrome("python"))
+        self.assertFalse(is_palindrome("This is not a palindrome"))
         
 if __name__ == '__main__':
     unittest.main()
